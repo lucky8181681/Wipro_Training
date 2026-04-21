deaad9542650dd6d088878fd16151302d02308f9
Sneha <snehalucky@gmail.com>
2026-04-21 17:15:05 +0530

    Added clean Day6 project

---
+++ Day6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MathOperation op = DelegateDemo.Add;
+        int result = op(10, 5);
+        Console.WriteLine("Addition: " + result);
+
+        op = DelegateDemo.Subtract;
+        result = op(10, 5);
+        Console.WriteLine("Subtraction: " + result);
+
+        op = DelegateDemo.Multiply;
+        result = op(10, 5);
+        Console.WriteLine("Multiplication: " + result);
+
+        op = DelegateDemo.Divide;
+        result = op(20, 5);
+        Console.WriteLine("Division: " + result);
+    }
+}
